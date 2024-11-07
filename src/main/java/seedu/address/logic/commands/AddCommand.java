8fc13abb5cae388e4224aaabc3c91e26e276c78f
nws321 <nws321@gmail.com>
2024-11-08 03:14:58 +0800

    Make display message more detailed

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -53,7 +53,7 @@ public class AddCommand extends Command {
             + PREFIX_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in either the unarchived or archived lists of the address book";
 
     private final Person toAdd;
 
